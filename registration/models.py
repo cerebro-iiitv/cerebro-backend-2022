e45ac77c6326d27ab84b797da4a5450e8a685f84
Krishna Gaur <gaurkrishna498@gmail.com>
2022-01-30 19:54:28 +0530

    Created models for team and individual participation

--- registration/models.py
+++ registration/models.py
@@ -10,24 +10,32 @@ class Dashboard(models.Model):
     action = models.BooleanField(default=True)
 
 
-class TeamStatus(models.Model):
+class IndividualParticipation(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="reg_individual")
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="reg_user")
+    registration_data = models.JSONField(null=True, blank=True)
+    submission_data = models.JSONField(null=True, blank=True)
+
+
+class TeamParticipation(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="reg_team")
+    team_creator = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="team_creator_acc")
+    team_name = models.CharField(max_length=52, blank=False)
     current_size = models.IntegerField(default=1)
     is_full = models.BooleanField(default=False)
     team_code = models.CharField(max_length=15, default=None)
+    registration_data = models.JSONField(null=True, blank=True)
+    submission_data = models.JSONField(null=True, blank=True)
 
     def __str__(self):
         return self.team_code
 
 
 class TeamMember(models.Model):
-    account = models.ForeignKey(
-        Account, on_delete=models.CASCADE, related_name="user_team"
-    )
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="user_team")
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="member")
-    team = models.ForeignKey(
-        TeamStatus, on_delete=models.CASCADE, related_name="team_member"
-    )
+    team = models.ForeignKey(TeamParticipation, on_delete=models.CASCADE, related_name="team_member")
+    registration_data = models.JSONField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.event.title + " | " + self.team.team_code
