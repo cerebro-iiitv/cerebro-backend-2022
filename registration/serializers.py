23462599e9fc0ccb1399264fe47687f34ffb53d2
Krishna Gaur <gaurkrishna498@gmail.com>
2022-01-30 19:57:06 +0530

    Added serializer for team, team member and individual participation model

--- registration/serializers.py
+++ registration/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
+from registration.models import TeamMember, TeamParticipation, IndividualParticipation
 
-from registration.models import TeamMember
 
+class IndividualParticipationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IndividualParticipation
+        fields = "__all__"
+        read_only_fields = ["account"]
 
 class TeamMemberSerializer(serializers.ModelSerializer):
     team_code = serializers.CharField(required=False, source="team.team_code")
 
     class Meta:
         model = TeamMember
-        fields = ["account", "event", "team_code"]
+        fields = "__all__"
+        read_only_fields = ["account", "event", "team"]
+
+class TeamParticipationSerializer(serializers.ModelSerializer):
+    team_creator = TeamMemberSerializer()
+    class Meta:
+        model = TeamParticipation
+        fields = "__all__" 
+        read_only_fields = ["team_creator", "current_size", "is_full", "team_code"]
+
+
