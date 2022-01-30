545072d04cf79fcc4847b16fe9ceadfd353762c4
Krishna Gaur <gaurkrishna498@gmail.com>
2022-01-30 21:18:33 +0530

    modified team model

--- registration/models.py
+++ registration/models.py
@@ -19,12 +19,11 @@ class IndividualParticipation(models.Model):
 
 class TeamParticipation(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="reg_team")
-    team_creator = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="team_creator_acc")
+    team_captain = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="team_creator_acc")
     team_name = models.CharField(max_length=52, blank=False)
     current_size = models.IntegerField(default=1)
     is_full = models.BooleanField(default=False)
     team_code = models.CharField(max_length=15, default=None)
-    registration_data = models.JSONField(null=True, blank=True)
     submission_data = models.JSONField(null=True, blank=True)
 
     def __str__(self):
