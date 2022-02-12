48b3d6a7c2c5a90a82bfcb4d781f6a8f1711747c
Krishna Gaur <gaurkrishna498@gmail.com>
2022-02-12 22:15:19 +0530

    PasswordTokenCheckApi removed

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,8 +14,6 @@ urlpatterns = [
     path('email-verify/', views.VerifyEmail.as_view(), name="email-verify"),
     path('request-reset-password/', views.RequestPasswordReset.as_view(),
          name="request-reset-password"),
-    path('password-reset/<uidb64>/<token>/',
-         views.PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
     path('password-reset-complete/', views.SetNewPasswordAPIView.as_view(),
          name='password-reset-complete')
 ]
