d3bb880b41e6521dec09225d9212c97e7ce207ad
Krishna Gaur <gaurkrishna498@gmail.com>
2022-02-12 22:45:54 +0530

    segregated response data by team

--- team/views.py
+++ team/views.py
@@ -1,4 +1,10 @@
+import imp
+import stat
+from typing import OrderedDict
+from httplib2 import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from rest_framework.response import Response
 
 from team.models import Team
 from team.serializers import TeamSerializer
@@ -14,3 +20,24 @@ class TeamViewSet(ModelViewSet):
             return Team.objects.filter(team=team).order_by("priority")
 
         return Team.objects.all().order_by("priority")
+
+    def list(self, request, *args, **kwargs):
+        team_choices = [
+            ("core", "Core"),
+            ("core-support", "Core Support"),
+            ("web", "Web Dev"),
+            ("design", "Design"),
+            ("video-editing", "Video Editing"),
+            ("pr", "PR"),
+            ("gaming", "Gaming"),
+        ]
+
+        data = dict()
+
+        for choice in team_choices:
+            team = Team.objects.filter(team=choice[0]).order_by("priority")
+            data[choice[1]] = self.serializer_class(team, many=True).data
+
+        print(data)
+
+        return Response(data, status=status.HTTP_200_OK)
