090a92266b5d13bc03406cda7d2f3948b9b76d3a
Krishna Gaur <gaurkrishna498@gmail.com>
2022-02-12 22:11:34 +0530

    modified password reset url

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
          name="request-reset-password"),
     path('password-reset/<uidb64>/<token>/',
          views.PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
-    path('password-reset-complete', views.SetNewPasswordAPIView.as_view(),
+    path('password-reset-complete/', views.SetNewPasswordAPIView.as_view(),
          name='password-reset-complete')
 ]
 
