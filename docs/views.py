3df3f1fbfb01d6badbdf44676e4dd718695d9cce
Krishna Gaur <gaurkrishna498@gmail.com>
2022-02-19 17:52:23 +0530

    handeled pdf upload for same email

--- docs/views.py
+++ docs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from httplib2 import Response
 from rest_framework.viewsets import ModelViewSet
 from accounts import serializers
 from docs.models import PDF
@@ -6,7 +7,9 @@ from docs.serializers import FileUploadSerializer, ProofFileUploadSerializer
 from rest_framework.parsers import FormParser, MultiPartParser
 from accounts.models import Account
 from rest_framework.permissions import AllowAny, IsAuthenticated
-# Create your views here.
+from rest_framework import status
+
+
 class FileUploadViewSet(ModelViewSet):
     
     queryset = PDF.objects.all()
@@ -17,6 +20,22 @@ class FileUploadViewSet(ModelViewSet):
     
     def list(self, request, *args, **kwargs):
         return super(FileUploadViewSet, self).list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
+        email = serializer.validated_data.get("email")
+
+        try:
+            prev_pdf = PDF.objects.get(email=email)
+            prev_pdf.delete()
+        except PDF.DoesNotExist:
+            pass    
+        
+        return super().create(request, *args, **kwargs)
     
     def get_permissions(self):
         try:
