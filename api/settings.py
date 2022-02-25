4baff6c53d5bbc48e5455ea8d4d0253260cffde9
mugdha273 <mugdhasharma0327@gmail.com>
2022-02-25 23:06:45 +0530

    DEFAULT_PERMISSION_CLASSES set to AllowAny

--- api/settings.py
+++ api/settings.py
@@ -127,7 +127,7 @@ REST_FRAMEWORK = {
         "accounts.authentication.MultipleTokenAuthentication",
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ]
 }
 
