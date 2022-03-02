6984e992dd0321f91ff49f5fefee53a72eea8d2a
mugdha273 <mugdhasharma0327@gmail.com>
2022-03-02 19:43:13 +0530

    data validation with regex

--- registration/utils.py
+++ registration/utils.py
@@ -1,10 +1,10 @@
+import re
+
 from distutils.log import error
 from events.models import Event
 
 def validate_registration_data(registration_attributes, registration_data):
     #Checking if the event require registration details
-
-    
 
     if registration_attributes is not None:
 
@@ -18,27 +18,11 @@ def validate_registration_data(registration_attributes, registration_data):
             error_message = {"error": "Invalid details provided for event registration"}
             return error_message
         
-        #Iterate through each registration details and verify the input type provided
         for key in registration_data.keys():
-            if registration_attributes.get(key) == "int":
-                if type(registration_data.get(key)) is not int:
-                        error_message = {"error": "Invalid details provided for event registration"}
-                        return error_message
 
-            elif registration_attributes.get(key) == "string":
-                if type(registration_data.get(key)) is not str:
-                    error_message = {"error": "Invalid details provided for event registration"}
-                    return error_message
-
-            elif registration_attributes.get(key) == "float":
-                if type(registration_data.get(key)) is not float:
-                    error_message = {"error": "Invalid details provided for event registration"}
-                    return error_message
-            
-            elif registration_attributes.get(key) == "boolean":
-                if type(registration_data.get(key)) is not bool:
-                    error_message = {"error": "Invalid details provided for event registration"}
-                    return error_message
+            if not (re.fullmatch(registration_attributes.get(key), registration_data.get(key))):
+                error_message = {"error": "Invalid details provided for event registration"}
+                return error_message
 
     else:
         if registration_data is not None:
@@ -65,25 +49,10 @@ def validate_submission_data(submission_attributes, submission_data):
         
         #Iterate through each submission details and verify the input type provided
         for key in submission_data.keys():
-            if submission_attributes.get(key) == "int":
-                if type(submission_data.get(key)) is not int:
-                        error_message = {"error": "Invalid details provided for event submission"}
-                        return error_message
-
-            elif submission_attributes.get(key) == "string":
-                if type(submission_data.get(key)) is not str:
-                    error_message = {"error": "Invalid details provided for event submission"}
-                    return error_message
-
-            elif submission_attributes.get(key) == "float":
-                if type(submission_data.get(key)) is not float:
-                    error_message = {"error": "Invalid details provided for event submission"}
-                    return error_message
-            
-            elif submission_attributes.get(key) == "boolean":
-                if type(submission_data.get(key)) is not bool:
-                    error_message = {"error": "Invalid details provided for event submission"}
-                    return error_message
+                
+            if not (re.fullmatch(submission_attributes.get(key), submission_data.get(key))):
+                error_message = {"error": "Invalid details provided for event submission"}
+                return error_message
 
     else:
         if submission_data is not None:
